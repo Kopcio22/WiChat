82ab493be3e92fcdc955a057ac2e3246b88a5263
Maciej <kopermaciek3@gmail.com>
2020-05-27 23:40:49 +0200

    init

---
+++ BE_API/modele/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+namespace BE_API.modele
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
